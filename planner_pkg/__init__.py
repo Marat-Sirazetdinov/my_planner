1131e40f4e2f81bf277bb638e8c103ccfc9a27ee
Marat-Sirazetdinov <kakahavupvup20005@gmail.com>
2026-02-16 22:34:29 +0300

    Delete planner_pkg directory

--- planner_pkg/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Инициализация пакета My planner.
-Предоставляет быстрый доступ к основным классам: Event, Reminder, Planner.
-"""
-from .events import Event
-from .reminders import Reminder
-from .planner import Planner
