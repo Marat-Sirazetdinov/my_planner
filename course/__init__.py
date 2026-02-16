b976eb8457ed635bb308fb473058997dd4b4ba20
Marat-Sirazetdinov <kakahavupvup20005@gmail.com>
2026-02-16 21:51:45 +0300

    Delete course directory

--- course/__init__.py
+++
@@ -1,3 +0,0 @@
-from events import Event
-from reminders import Reminder
-from planner import Planner
--- course/reminders.py
+++
@@ -1,6 +0,0 @@
-class Reminder:
-    def __init__(self, minutes: int):
-        self.minutes = minutes
-
-    def __repr__(self):
-        return f"Напомнить за {self.minutes} мин."
