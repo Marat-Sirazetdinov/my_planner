d4b2fc11fa947592ac386ea75a8af97313e1160a
Marat-Sirazetdinov <kakahavupvup20005@gmail.com>
2026-02-17 18:06:59 +0300

    Update __init__.py

--- my_planner/__init__.py
+++ my_planner/__init__.py
@@ -2,6 +2,6 @@
 Инициализация пакета My planner.
 Предоставляет быстрый доступ к основным классам: Event, Reminder, Planner.
 """
-from events import Event
-from reminders import Reminder
-from planner import Planner
+from .events import Event
+from .reminders import Reminder
+from .planner import Planner
